75a5cfbba5ae27eaca958e3494b44b5fe9ff2c54
Brian 'bdougie' Douglas <ilikerobot@gmail.com>
2019-08-10 13:47:57 -0700

    add codecov

--- Gemfile
+++ Gemfile
@@ -2,5 +2,8 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
+gem 'codecov', :require => false, :group => :test
+
+
 # Specify your gem's dependencies in marky_markdown.gemspec
 gemspec
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,3 +2,9 @@ $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "marky_markdown"
 
 require "minitest/autorun"
+
+require 'simplecov'
+SimpleCov.start
+
+require 'codecov'
+SimpleCov.formatter = SimpleCov::Formatter::Codecov
